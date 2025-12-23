ec34a7807da3d8809799b0e7a85d1a40edac64d4
Patrick <patrickcaminhasm@gmail.com>
2025-12-23 08:32:20 -0300

    spring initializer

---
+++ src/test/java/br/com/patrickcaminhas/gestaoEmpresarial/GestaoEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrickcaminhas.gestaoEmpresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
